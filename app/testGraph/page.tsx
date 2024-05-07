5b6cd98f9207ced028a04ed094ab2f820ccb5223
Grangy <m.dolia2017@yandex.ru>
2024-05-07 16:16:35 +0300

    new2

--- app/testGraph/page.tsx
+++ app/testGraph/page.tsx
@@ -1,25 +1,19 @@
-// lk/page.tsx
-"use client"
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import SidebarMenu from '../components/SidebarMenu';
-import PersonalCabinetContent from '../components/PersonalCabinetContent';
+// testGraph/page.tsx
+"use client";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import SidebarMenu from "../components/SidebarMenu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
-const GET_PRODUCT_CATEGORIES = gql`
-  query getProductCategories {
-    productCategories {
-      id
-      name
-      desc
-      _count {
-        products
-      }
-    }
+const SIMPLE_QUERY = gql`
+  query {
+    serverStatus
   }
 `;
 
 const PersonalCabinet = () => {
-  const { data, loading, error } = useQuery(GET_PRODUCT_CATEGORIES);
+  const { data, loading, error } = useQuery(SIMPLE_QUERY);
 
   React.useEffect(() => {
     if (!loading && data) {
@@ -33,17 +27,15 @@ const PersonalCabinet = () => {
       <SidebarMenu />
       <div className="text-center mt-5 flex-grow">
         {loading ? (
-          <p>Loading...</p>
+          <FontAwesomeIcon
+            icon={faCircleNotch}
+            spin
+            className="text-6xl text-green-500"
+          />
         ) : error ? (
           <p>Error: {error.message}</p>
         ) : (
-          data.productCategories.map((category: { id: React.Key | null | undefined; name: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; desc: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; _count: { products: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; }; }) => (
-            <div key={category.id}>
-              <h2>{category.name}</h2>
-              <p>{category.desc}</p>
-              <p>Number of products: {category._count.products}</p>
-            </div>
-          ))
+          <p>Server Status: {data.serverStatus}</p>
         )}
       </div>
     </div>
