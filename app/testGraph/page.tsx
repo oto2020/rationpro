b3d420e2a8cde6074d898fffb7e3a56a9346d4dc
Grangy <m.dolia2017@yandex.ru>
2024-05-07 17:06:20 +0300

    Final

--- app/testGraph/page.tsx
+++ app/testGraph/page.tsx
@@ -42,7 +42,15 @@ const PersonalCabinet = () => {
         ) : error ? (
           <p>Error: {error.message}</p>
         ) : (
-          <p>Server Status: {data.serverStatus}</p>
+          <div className="p-4">
+            {data.productCategories.map((category: { id: React.Key | null | undefined; name: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; desc: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; _count: { products: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; }; }) => (
+              <div key={category.id} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h2 className="font-bold text-2xl mb-2">{category.name}</h2>
+                <p className="text-gray-700 text-base">{category.desc}</p>
+                <p className="mt-2 text-gray-500">Number of products: {category._count.products}</p>
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -2,12 +2,13 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://ration.phys.su/graphql',  // Укажите URL вашего GraphQL сервера
+    uri: 'https://ration.phys.su/graphql/',  // Укажите URL вашего GraphQL сервера
+    headers: {
+      'content-type': 'application/json'
+      // Добавьте здесь любые другие заголовки, которые вам нужны
+    }
   }),
   cache: new InMemoryCache()
 });
-
-
-
 
 export default client;
