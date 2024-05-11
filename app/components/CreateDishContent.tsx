5351ae5575a345f577e925ccc06d2fec3b8bd593
Grangy <m.dolia2017@yandex.ru>
2024-05-11 03:08:01 +0300

    кнопка

--- app/components/CreateDishContent.tsx
+++ app/components/CreateDishContent.tsx
@@ -212,7 +212,7 @@ const CreateDishContent = () => {
       {Object.keys(COOKING_METHODS).map((method) => {
         const cookingMethodKey = method as CookingMethods;
         return (
-          <button key={cookingMethodKey} className={`relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group ${cookingMethod === cookingMethodKey ? "bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white" : "text-gray-900 bg-white"} focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800`} onClick={() => setCookingMethod(cookingMethodKey)}>
+          <button key={cookingMethodKey} className={`relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group ${cookingMethod === cookingMethodKey ? "bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white" : "text-gray-900 bg-white"} focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-200`} onClick={() => setCookingMethod(cookingMethodKey)}>
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">{cookingMethodKey}</span>
           </button>
         );
